dec96d14bf708553774c02aa8a221cfb19fab6e3
Sam Houston <shouston3@me.com>
2020-02-17 10:11:46 +0000

    use 'else if' pattern over a return early pattern

--- archive.js
+++ archive.js
@@ -7,32 +7,27 @@ if (args[0] === 'm') {
   if (args[1] === 'put') {
     MArchive.put(args.slice(2, args.length));
     return;
-  }
-
-  if (args[1] === 'get' && args.length === 2) {
+  } else if (args[1] === 'get' && args.length === 2) {
     MArchive.get();
     return;
+  } else {
+    throw 'unknown args passed to ./archive.js m';
   }
-
-  throw 'unknown args passed to ./archive.js m';
-}
-
-if (args[0] === 'mg') {
+} else if (args[0] === 'mg') {
   if (args[1] === 'put') {
     MgArchive.put(args.slice(2, args.length));
     return;
-  }
-
-  if (args[1] === 'get' && args.length === 3) {
+  } else if (args[1] === 'get' && args.length === 3) {
     if (args[2] === 'all') {
       MgArchive.get('all');
+      return;
     } else {
       MgArchive.get(args[2]);
+      return;
     }
-    return;
+  } else {
+    throw 'unknown args passed to ./archive.js mg';
   }
-
-  throw 'unknown args passed to ./archive.js mg';
+} else {
+  throw 'unknown first arg passed to ./archive.js';
 }
-
-throw 'unknown first arg passed to ./archive.js';
