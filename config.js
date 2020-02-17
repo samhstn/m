ec454fb1795adcd8f63c454c15e89af945e524e5
Sam Houston <shouston3@me.com>
2020-02-17 10:12:22 +0000

    add documentation and clarify class usage

--- config.js
+++ config.js
@@ -3,23 +3,23 @@
 const path = require('path');
 const NODE_ENV = process.env.NODE_ENV;
 
-const MgProcesses = require('./lib/mg_process.js');
+const { MgProcess, MgProcessInternal } = require('./lib/mg_process.js');
 
 const archiveDir =
   NODE_ENV === 'test' ? 
   path.resolve(__dirname, '.test_archive') :
   path.resolve(__dirname, '.archive');
 
-const MgProcess =
+const MgProcess_ =
   NODE_ENV === 'test' ?
-  MgProcesses.MgProcessInternal :
-  MgProcesses.MgProcess
+  MgProcessInternal :
+  MgProcess;
 
 const config = {
   archiveDir: archiveDir,
   mArchiveFile: path.join(archiveDir, 'm_archive.json'),
   mgArchiveFile: path.join(archiveDir, 'mg_archive.json'),
-  MgProcess: MgProcess
+  MgProcess: MgProcess_
 }
 
 module.exports = config;
